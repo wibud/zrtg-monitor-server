9f837e305d74451fee45fc854010ff2e20809cb6
大果 <liuxiong.lx@alibaba-inc.com>
2015-08-16 16:36:37 +0800

    page 判断

--- controllers/common.js
+++ controllers/common.js
@@ -1,12 +1,14 @@
 
 
 exports.home = function* (){
-	yield this.render('home');
+	yield this.render('home', {
+		page: 'home'
+	});
 };
 
 exports.notFound = function* () {
 
-	this.status = 400;
+	this.status = 404;
 	yield this.render('404');
 
 };
